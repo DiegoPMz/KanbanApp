437dab225a4734c911adb2bcc6f0acf6b16652ef
DiegoPmz <diego.pmz08@gmail.com>
2025-10-04 21:50:17 -0600

    se agregó Tailwind y se movió el código antiguo a legacy
    - Se agregó Tailwind CSS para el estilizado del proyecto.
    - Se movió el código antiguo a la carpeta `legacy/` para mantenerlo separado.
    - Se excluyó la carpeta `legacy/` de `tsconfig.json`, `tsconfig.app.json` y de ESLint.

--- eslint.config.js
+++ eslint.config.js
@@ -5,9 +5,9 @@ import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'legacy'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.strict],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
