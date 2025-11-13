c9c05ee3093a47a80b3c06b1be0f549b3a2a0f4f
DiegoPmz <diego.pmz08@gmail.com>
2025-11-13 01:54:05 -0600

    Se Reestructuro el proyecto para utilizar estilos y componentes compartidos; se añadió estilos globales y funciones de utilidad.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./core/styles/global.css";
+import "./shared/styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
